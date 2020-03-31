2900a10fe33565687bdc0c04ecce70492ecaee61
Kevin Koech <kevin.koech@aurora.app>
2020-03-31 18:03:34 +0300

    build electron

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 const electron = require('electron');
-const path = require('path');
 
 const { app, BrowserWindow } =  electron;
 
-const icon = electron.nativeImage.createFromPath(
-    path.resolve('public/favicon.icns')
-)
 const createWindow = () => {
     let win = new BrowserWindow({
         minWidth: 800,
         minHeight: 600,
-        icon,
     });
     win.loadURL('https://my-resume-92231.firebaseapp.com').then();
 }
-
-app.dock.setIcon(icon);
 
 app.whenReady().then(createWindow);
 
